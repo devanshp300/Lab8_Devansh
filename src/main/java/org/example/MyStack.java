6ed7decb0edf0397b123f5bc31555d4204f93d54
Devansh291001 <devanshpatel2910@icloud.com>
2024-03-06 18:34:22 -0500

    created an instance variable of type ArrayList - delegate initialized as "this"

--- src/main/java/org/example/MyStack.java
+++ src/main/java/org/example/MyStack.java
@@ -5,6 +5,13 @@ import java.util.EmptyStackException;
 
 public class MyStack<E> extends ArrayList<E> {
 
+    private ArrayList<E> delegate;
+
+    public MyStack(){
+        this.delegate = this;
+    }
+
+
     public void push(E e) {
         add(e);
     }
